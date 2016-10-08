0140ac377c6fba8a12541ac5edb7fe83c40cb093
NeoliTik <NeoliTik>
2016-10-08 05:29:46 +0200

    code optimize

--- src/controle.c
+++ src/controle.c
@@ -5,9 +5,10 @@
  */
 
 #include "header.h"
-extern bool buttonUP = true;
 
 void controle(SceCtrlData pad, int *navOffset, bool *exit, Plugins plugins[]) {
+	static bool buttonUP = true;
+
 	if (pad.buttons & SCE_CTRL_UP && buttonUP) {
 		*navOffset -= 1;
 		buttonUP = false;
@@ -16,7 +17,7 @@ void controle(SceCtrlData pad, int *navOffset, bool *exit, Plugins plugins[]) {
 		buttonUP = false;
 	} else if (pad.buttons & SCE_CTRL_CROSS && buttonUP) {
 		bool curStat = plugins[*navOffset].active;
-		plugins[*navOffset].active = (curStat == true ? false : true);
+		plugins[*navOffset].active = (curStat ? false : true);
 		putConfigs(plugins);
 		dirPlugins(plugins, *navOffset);
 		buttonUP = false;
