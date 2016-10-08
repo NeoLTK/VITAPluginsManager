06fca848f6fb22edeb62caa27514414e76b265ec
NeoliTik <NeoliTik>
2016-10-08 04:37:26 +0200

    ui improve

--- readme.txt
+++ readme.txt
@@ -1,11 +1,12 @@
-VITAPluginsManager V0.3 for vitamin / mai dump
+VITAPluginsManager for vitamin / mai dump
 
 Features :
 
-* Enable write line in game.txt and change location of plugin ( "ux0:/plugins" )
-* Disable remove line in game.txt and change location of plugin ( "ux0:/plugins_disabled" )
+* Enable  : write line in game.txt and change location of plugin ( "ux0:/plugins" )
+* Disable : remove line in game.txt and change location of plugin ( "ux0:/plugins_disabled" )
 
 Require :
 
 * Henkaku HEN
-* plugins suprx on ux0/plugins or ux0/plugins_disabled
+* VitaShell / MoleculeShell
+* Plugins suprx on "ux0/plugins" or "ux0/plugins_disabled"
--- src/menu.c
+++ src/menu.c
@@ -6,6 +6,16 @@
 
 #include "header.h"
 
+void uiDraw(){
+	font_draw_string(20, 10, RGBA8(0, 255, 0, 255), "Plugins Name");
+	font_draw_string(775, 10, RGBA8(0, 255, 0, 255), "Status");
+	draw_rectangle(0, 35, 960, 1, GREEN);
+
+	draw_rectangle(0, 510, 960, 1, GREEN);
+	font_draw_stringf(5, 520, RGBA8(0, 255, 0, 255), "VITAPluginsManager | %s", version);
+	font_draw_string(SCREEN_W - 100, 520, RGBA8(0, 255, 0, 255), "NeoLTK");
+}
+
 void menuDraw(Plugins plugins[], int *navOffset) {
 	int i;
 	for (i = 0;  plugins[i].name != '\0'; i++) 
@@ -23,16 +33,17 @@ void menuDraw(Plugins plugins[], int *navOffset) {
 		for (int i = 0; plugins[i].name != '\0'; i++) {
 
 			if (*navOffset == i) {
-				font_draw_string(25, 35 + (i * 25), RGBA8(255, 255, 255, 255), plugins[i].name);
-				font_draw_string(355, 35 + (i * 25), RGBA8(255, 255, 255, 255), (plugins[i].active == true ? "[ENABLED]" : "[DISABLED]"));
+				font_draw_string(20, 55 + (i * 25), RGBA8(255, 255, 255, 255), "*");
+				font_draw_string(50, 55 + (i * 25), RGBA8(255, 255, 255, 255), plugins[i].name);
+				font_draw_string(750, 55 + (i * 25), RGBA8(255, 255, 255, 255), (plugins[i].active ? "[ENABLED] *" : "[DISABLED] *"));
 
 			} else {
-				font_draw_string(20, 35 + (i * 25), RGBA8(0, 255, 0, 255),  plugins[i].name);
-				font_draw_string(350, 35 + (i * 25), RGBA8(0, 255, 0, 255), (plugins[i].active == true ? "[ENABLED]" : "[DISABLED]"));
+				font_draw_string(50, 55 + (i * 25), RGBA8(0, 255, 0, 255),  plugins[i].name);
+				font_draw_string(750, 55 + (i * 25), RGBA8(0, 255, 0, 255), (plugins[i].active ? "[ENABLED]" : "[DISABLED]"));
 			}
 		}
 	}
 
-	
-	font_draw_stringf(0, 10, RGBA8(0, 255, 0, 255), "VITAPluginsManager by NeoLTK | %s ", version);
+
+	uiDraw();
 }
