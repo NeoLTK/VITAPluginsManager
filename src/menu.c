3106c554790e29410dcfc2973d85432ac844bdc1
NeoliTik <NeoliTik>
2016-10-08 06:37:45 +0200

    New UI improve

--- src/menu.c
+++ src/menu.c
@@ -12,8 +12,24 @@ void uiDraw(){
 	draw_rectangle(0, 35, 960, 1, GREEN);
 
 	draw_rectangle(0, 510, 960, 1, GREEN);
-	font_draw_stringf(5, 520, RGBA8(0, 255, 0, 255), "VITAPluginsManager | %s", version);
-	font_draw_string(SCREEN_W - 100, 520, RGBA8(0, 255, 0, 255), "NeoLTK");
+	font_draw_stringf(5, 520, RGBA8(0, 255, 0, 255), "VITAPluginsManager |%s", version);
+	font_draw_string(SCREEN_W - 130, 520, RGBA8(0, 255, 0, 255), "-|NeoLTK");
+}
+
+uint32_t epileptik (){
+	static bool offset = true;
+    static uint32_t value = 0;
+
+	if (value < 255 && offset)
+		return value = value + 3;
+	else if (value > 0 && !offset)
+		return value = value - 3;
+	else if (value == 0)
+		offset = true;
+	else if (value == 255)
+		offset = false;
+
+	return value;
 }
 
 void menuDraw(Plugins plugins[], int *navOffset) {
@@ -22,6 +38,7 @@ void menuDraw(Plugins plugins[], int *navOffset) {
 		continue;
 
 	int size = i;
+	uint32_t epileptikColor = epileptik();
 
 	if (size > 0) {
 		if (*navOffset > size-1) {
@@ -33,9 +50,10 @@ void menuDraw(Plugins plugins[], int *navOffset) {
 		for (int i = 0; plugins[i].name != '\0'; i++) {
 
 			if (*navOffset == i) {
-				font_draw_string(20, 55 + (i * 25), RGBA8(255, 255, 255, 255), "*");
+				font_draw_string(20, 55 + (i * 25), RGBA8(epileptikColor, epileptikColor, epileptikColor, 255), "*");
 				font_draw_string(50, 55 + (i * 25), RGBA8(255, 255, 255, 255), plugins[i].name);
-				font_draw_string(750, 55 + (i * 25), RGBA8(255, 255, 255, 255), (plugins[i].active ? "[ENABLED] *" : "[DISABLED] *"));
+				font_draw_string(750, 55 + (i * 25), RGBA8(255, 255, 255, 255), (plugins[i].active ? "[ENABLED]" : "[DISABLED]"));
+				font_draw_string(925, 55 + (i * 25), RGBA8(epileptikColor, epileptikColor, epileptikColor, 255), "*");
 
 			} else {
 				font_draw_string(50, 55 + (i * 25), RGBA8(0, 255, 0, 255),  plugins[i].name);
@@ -47,3 +65,4 @@ void menuDraw(Plugins plugins[], int *navOffset) {
 
 	uiDraw();
 }
+
